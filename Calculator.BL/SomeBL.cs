30bfef4b9dad91b0a41dfe1e32403940c2e2567f
igor <35834078+igorpuchilo@users.noreply.github.com>
2018-01-26 21:00:13 +0300

    ready

---
+++ Calculator.BL/SomeBL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.BL
+{
+    public class SomeBL
+    {
+        public int Summ(int a, int b)
+            {
+            int c = a + b;
+            return c;
+            }
+    }
+}
---
+++ Calculator/Controllers/ValuesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Calculator.BL;
+namespace Calculator.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        public IHttpActionResult Get(int? a, int? b)
+        {
+            if ((a.HasValue) && (b.HasValue))
+            {
+                //update
+                SomeBL objsum = new SomeBL();
+                return Ok(objsum.Summ(a.Value, b.Value));
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
