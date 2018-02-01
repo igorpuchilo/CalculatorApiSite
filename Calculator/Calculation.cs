71b1d7f03c7dd04721eddf256e671202750e5508
igor <35834078+igorpuchilo@users.noreply.github.com>
2018-02-02 00:24:42 +0300

    EntityFrameWork

---
+++ Calculator/Calculation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Calculator
+{
+    public class Calculation
+    {
+        public int Id { get; set; }
+        public string Expression { get; set; }
+        public int Result { get; set; }
+        public String CreatedOn { get; set; }
+    }
+}
---
+++ Calculator/CalculationContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Calculator
+{
+    public class CalculationContext : DbContext
+    {
+        public CalculationContext()
+           : base("DbConnection")
+        { }
+
+        public DbSet<Calculation> Calculation { get; set; }
+    }
+}
