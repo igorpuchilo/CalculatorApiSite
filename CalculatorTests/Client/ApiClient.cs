92b8e0fd57a90fdc35ee1401208a521a6c94dcb5
igorpuchilo <35834078+igorpuchilo@users.noreply.github.com>
2018-03-05 11:09:54 +0300

    Http:0000

---
+++ CalculatorTests/Client/ApiClient.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CalculatorTests.Client
+{
+    class ApiClient : IDisposable
+    {
+        private readonly JsonSerializerSettings serializerSettings = new JsonSerializerSettings
+        {
+            NullValueHandling = NullValueHandling.Ignore,
+            DateFormatHandling = DateFormatHandling.IsoDateFormat,
+            TypeNameHandling = TypeNameHandling.All
+        };
+        public string Raw { get; internal set; }
+        private class HttpClientEx : HttpClient
+        {
+            //Sad smile
+        }
+        private HttpClientEx client = new HttpClientEx
+        {
+            BaseAddress = new Uri ("http://localhost:49881/"),
+            Timeout = new TimeSpan(0, 10, 0)
+        };
+        public void Dispose()
+        {
+            if (this.client != null)
+            {
+                this.client.Dispose();
+                this.client = null;
+            }
+        }
+        public HttpResponseMessage Get(string url)
+        {
+            return GetAsync(url).Result;
+        }
+        public async Task<HttpResponseMessage> GetAsync(string url)
+        {
+            return await client.GetAsync(url);
+        }
+
+        public T Deserialize<T>()
+        {
+            if (string.IsNullOrWhiteSpace(Raw))
+                return default(T);
+            return JsonConvert.DeserializeObject<T>(Raw, this.serializerSettings);
+        }
+    }
+}
